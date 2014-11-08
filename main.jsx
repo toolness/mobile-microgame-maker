4e16130a5ab5fdd4810e6c698dd1fb7dfec9b314
Atul Varma <varmaa@gmail.com>
2014-11-08 16:20:27 -0500

    add spreadsheet-to-spritesheet.js.

--- main.jsx
+++ main.jsx
@@ -94,44 +94,10 @@
     if (/[&|?]local=1/.test(window.location.search))
       return render();
 
-    Tabletop.init({
-      key: '15P3ABqc128s1z4vA2Ln1EdrFTXPxZ8YMaiW1w3o1qgs',
-      callback: function(data) {
-        var spritesheets = [];
-        var animations = {};
-        var lastSpritesheetKey;
-        data.forEach(function(row) {
-          var key = row.spritesheetkey || lastSpritesheetKey;
-          if (row.spritesheetkey) {
-            spritesheets.push({
-              key: key,
-              url: row.url,
-              frameWidth: parseInt(row.framewidth),
-              frameHeight: parseInt(row.frameheight)
-            });
-            lastSpritesheetKey = key;
-            animations[key] = [];
-          }
-          if (row.animationkey && key) {
-            animations[key].push({
-              name: row.animationkey,
-              frames: row.animationframes.split(',').map(function(frame) {
-                return parseInt(frame.trim());
-              }),
-              frameRate: parseInt(row.animationfps),
-              loop: row.animationloop == "TRUE"
-            });
-          }
-        });
-        render({
-          spritesheets: spritesheets,
-          animations: animations
-        });
-      },
-      simpleSheet: true
-    });
+    var SPREADSHEET_KEY = '15P3ABqc128s1z4vA2Ln1EdrFTXPxZ8YMaiW1w3o1qgs';
+    spreadsheetToSpritesheet(SPREADSHEET_KEY, render);
   }
-  
+
   if (document.readyState == 'loading')
     document.addEventListener('DOMContentLoaded', start, false);
   else
