afff6ed15af0cbb6f10e4eed22f160629518e120
Atul Varma <varmaa@gmail.com>
2014-11-21 05:24:55 -0500

    don't create a new phaser iframe per phaser state change.

--- stage.jsx
+++ stage.jsx
@@ -11,30 +11,20 @@ define(function(require) {
       return {loading: true};
     },
     setPhaserState: function(newState) {
-      if (this.game) {
-        if (this.game.canvas) {
-          this.refs.placeholder.getDOMNode().getContext('2d').drawImage(
-            this.game.canvas,
-            0,
-            0
-          );
-        }
-        try {
-          this.game.destroy();
-        } catch (e) {
-          // This seems to happen sometimes, perhaps b/c the game
-          // wasn't yet fully initialized by the time we try to
-          // destroy it? Either way, it's probably not a big deal
-          // since the game is in an iframe that will be destroyed,
-          // so just log the error and move on.
-          console.log("error when destroying Phaser game", e);
-        }
-        this.game = null;
+      var hardReset = !newState || (this.iframe && !this.game);
+
+      if (this.game && this.game.canvas && newState) {
+        this.refs.placeholder.getDOMNode().getContext('2d').drawImage(
+          this.game.canvas,
+          0,
+          0
+        );
       }
 
-      if (this.iframe) {
+      if (this.iframe && hardReset) {
         this.iframe.parentNode.removeChild(this.iframe);
         this.iframe = null;
+        this.game = null;
       }
 
       if (!newState) return;
@@ -42,31 +32,43 @@ define(function(require) {
       var self = this;
       var originalCreate = newState.create;
       var document = this.refs.phaser.getDOMNode().ownerDocument;
-      var iframe = document.createElement('iframe');
 
       newState.create = function() {
         self.setState({loading: false});
         originalCreate.apply(this, arguments);
       };
 
-      iframe.setAttribute('src', 'phaser-frame.html');
-      iframe.onload = function() {
-        var Phaser = iframe.contentWindow.Phaser;
-        newState.Phaser = Phaser;
-        self.game = new Phaser.Game(
-          self.props.width,
-          self.props.height,
-          Phaser.CANVAS,
-          iframe.contentDocument.body,
-          newState
-        );
-      };
-      this.refs.phaser.getDOMNode().appendChild(iframe);
-      iframe.style.width = this.props.width + 'px';
-      iframe.style.height = this.props.height + 'px';
-      iframe.style.border = 'none';
+      if (!this.iframe) {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('src', 'phaser-frame.html');
+        iframe.onload = function() {
+          var Phaser = iframe.contentWindow.Phaser;
+          newState.Phaser = Phaser;
+          newState.id = 1;
+          self.game = new Phaser.Game(
+            self.props.width,
+            self.props.height,
+            Phaser.CANVAS,
+            iframe.contentDocument.body,
+            null
+          );
+          self.game.state.add(newState.id.toString(), newState, true);
+        };
+        this.refs.phaser.getDOMNode().appendChild(iframe);
+        iframe.style.width = this.props.width + 'px';
+        iframe.style.height = this.props.height + 'px';
+        iframe.style.border = 'none';
 
-      this.iframe = iframe;
+        this.iframe = iframe;
+      } else {
+        var oldState = this.game.state.getCurrentState();
+        newState.Phaser = oldState.Phaser;
+        newState.id = oldState.id + 1;
+        this.game.state.add(newState.id.toString(), newState, true);
+        this.game.state.remove(oldState.id.toString());
+        this.game.paused = false;
+        this.game.input.disabled = false;
+      }
       this.setState({loading: true});
     },
     componentDidUpdate: function(prevProps) {
