93932ebcf827a4bb12163ddc3d6b67c8eb91d55d
Atul Varma <varmaa@gmail.com>
2014-11-25 19:42:27 -0500

    more refactorings to main.js.

--- main.js
+++ main.js
@@ -3,46 +3,66 @@ require([
   "spreadsheet-to-spritesheet",
   "jquery"
 ], function(app, spreadsheetToSpritesheet, $) {
-  var qs = (function getQueryVariables(query) {
-    var result = {};
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
-      result[pair[0]] = decodeURIComponent(pair[1]);
-    }
-    return result;
-  })(window.location.search.substring(1));
-
-  function startApp(spriteLibrary) {
-    $('html').removeClass('loading');
-
-    var editor = app({
-      root: document.documentElement,
-      editorHolder: document.getElementById('editor-holder'),
-      modalHolder: document.getElementById('modal-holder'),
-      blocklyHolder: document.getElementById('blockly-holder'),
-      storage: window.sessionStorage,
-      storageKey: 'mmm_gamedata',
-      spriteLibrary: spriteLibrary
-    });
-
-    // For debugging via console only!
-    window.editor = editor;
-  }
-
-  $(function() {
-    var spreadsheet = qs.spreadsheet;
+  function importSpreadsheet(spreadsheet, options) {
+    var TIMEOUT = 5000;
+    var deferred = $.Deferred();
+    var timeout = null;
 
     if (spreadsheet && spreadsheet.match(/^[A-Za-z0-9]+$/)) {
       if (spreadsheet == 'on') {
         spreadsheet = '15P3ABqc128s1z4vA2Ln1EdrFTXPxZ8YMaiW1w3o1qgs';
       }
 
-      spreadsheetToSpritesheet(spreadsheet, function(spriteLibrary) {
-        startApp(spriteLibrary);
+      timeout = window.setTimeout(function() {
+        timeout = null;
+        window.alert('Failed to retrieve spreadsheet ' + spreadsheet);
+        deferred.reject();
+      }, TIMEOUT);
+      spreadsheetToSpritesheet(spreadsheet, function(result) {
+        if (timeout === null) return;
+        clearTimeout(timeout);
+        options.spriteLibrary = result;
+        deferred.resolve();
       });
     } else {
-      startApp();
+      deferred.resolve();
     }
+
+    return deferred;
+  }
+
+  function startApp(options) {
+    $('html').removeClass('loading');
+
+    var editor = app($.extend(options, {
+      root: document.documentElement,
+      editorHolder: document.getElementById('editor-holder'),
+      modalHolder: document.getElementById('modal-holder'),
+      blocklyHolder: document.getElementById('blockly-holder'),
+      storage: window.sessionStorage,
+      storageKey: 'mmm_gamedata'
+    }));
+
+    // For debugging via console only!
+    window.editor = editor;
+  }
+
+  $(function() {
+    var options = {};
+    var qs = (function getQueryVariables(query) {
+      var result = {};
+      var vars = query.split("&");
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        result[pair[0]] = decodeURIComponent(pair[1]);
+      }
+      return result;
+    })(window.location.search.substring(1));
+
+    $.when(
+      importSpreadsheet(qs.spreadsheet, options)
+    ).then(function() {
+      startApp(options);
+    });
   });
 });
