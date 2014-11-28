795cbecc1dae513f9b27c8e50f0d523b94d8daec
Atul Varma <varmaa@gmail.com>
2014-11-28 08:45:08 -0500

    added more css-sprite tests

--- test/test-css-sprite.js
+++ test/test-css-sprite.js
@@ -21,4 +21,28 @@ defineTests([
     ok(!cs.getDOMNode().hasAttribute('style'));
     cs.unmountComponent();
   });
+
+  test("scales sprite when height > width", function() {
+    var cs = React.createElement(CssSprite, {
+      gameData: defaultGameData,
+      maxDimension: 10,
+      sprite: {name: 'example', key: 'fly', animation: 'flying'}
+    });
+    cs = React.addons.TestUtils.renderIntoDocument(cs);
+    equal(cs.getDOMNode().style.height, '10px');
+    equal(cs.getDOMNode().style.width.slice(0, 3), '8.6');
+    cs.unmountComponent();
+  });
+
+  test("scales sprite when width > height", function() {
+    var cs = React.createElement(CssSprite, {
+      gameData: defaultGameData,
+      maxDimension: 10,
+      sprite: {name: 'example', key: 'grumpaloomba', animation: 'grumpy'}
+    });
+    cs = React.addons.TestUtils.renderIntoDocument(cs);
+    equal(cs.getDOMNode().style.width, '10px');
+    equal(cs.getDOMNode().style.height.slice(0, 3), '9.7');
+    cs.unmountComponent();
+  });
 });
