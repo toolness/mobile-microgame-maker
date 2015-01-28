6770fe4e9fd8ed1f05ecf7d8374fb41095a30a3e
Atul Varma <varmaa@gmail.com>
2015-01-27 21:57:57 -0500

    upload gzipped version of main.js to s3.

--- bin/sync-to-s3.sh
+++ bin/sync-to-s3.sh
@@ -3,4 +3,8 @@
 node bin/build-optimized.js &&
   # The '/' after 'build' is REALLY important here.
   s3cmd sync --acl-public build/ s3://mmm.minica.de &&
+  gzip -c -9 build/main.js > build/main.js.gz &&
+  s3cmd put --acl-public -m application/javascript \
+            --add-header 'Content-Encoding:gzip' build/main.js.gz \
+            s3://mmm.minica.de/main.js &&
   s3cmd sync --acl-public assets/js/ s3://minicade-assets/js/
