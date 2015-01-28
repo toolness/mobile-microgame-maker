e2d2f39521a10880abf7d1a2db8576e9878925bf
Atul Varma <varmaa@gmail.com>
2015-01-27 22:21:21 -0500

    gzip and cache phaser js on s3.

--- bin/sync-to-s3.sh
+++ bin/sync-to-s3.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+export PHASER_VERSION=2.2.1
+
 node bin/build-optimized.js &&
   # The '/' after 'build' is REALLY important here.
   s3cmd sync --acl-public build/ s3://mmm.minica.de &&
@@ -7,4 +9,11 @@ node bin/build-optimized.js &&
   s3cmd put --acl-public -m application/javascript \
             --add-header 'Content-Encoding:gzip' build/main.js.gz \
             s3://mmm.minica.de/main.js &&
+  gzip -c -9 build/vendor/phaser-${PHASER_VERSION}.js > \
+       build/vendor/phaser-${PHASER_VERSION}.js.gz &&
+  s3cmd put --acl-public -m application/javascript \
+            --add-header 'Content-Encoding:gzip' \
+            --add-header="Cache-Control:max-age=86400" \
+            build/vendor/phaser-${PHASER_VERSION}.js.gz \
+            s3://mmm.minica.de/vendor/phaser-${PHASER_VERSION}.js &&
   s3cmd sync --acl-public assets/js/ s3://minicade-assets/js/
